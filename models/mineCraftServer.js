b2edb3e864048dbc082687f01673f87bd5a6fc2b
UmutAkkaya <uba1995a@gmail.com>
2015-11-29 16:41:12 -0500

    Decreased the fields that we have for a server since we can get only these so far

--- models/mineCraftServer.js
+++ models/mineCraftServer.js
@@ -5,27 +5,17 @@ var mongoose = require('mongoose');
  */
 var mineCraftServer = mongoose.Schema({
     ip: String,
-    domain: String,
-    port: String,
-    url: String,
-    info: {
-        name: String,
-        platform: String,
-        version: String, //ex. 1.8.0
-        type: String, //ex. SMP
-        maxPlayerNumber: Number
-    },
-    dateAdded: String,
-    geo: {
-        country: String,
-        city: String
-    },
-    lastUpdate: String,
-    upTime: Number,
-    online: Number,
-    offline: Number,
-    software: String, //ex. CraftBukkit on Bukkit 1.8.8-R0.1-SNAPSHOT
-    plugins: [String]
+    title: String,
+    gameMode: Number,
+    gameId: Number,
+    version: String,
+    plugins: String,
+    map: String,
+    numPlayersOnline: Number,
+    maxPlayers: Number,
+    fromIP: String,
+    port: Number,
+    onlineplayers: [String]
 });
 
 module.exports = mongoose.model('MineCraftServer', mineCraftServer);
