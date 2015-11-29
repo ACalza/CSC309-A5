9ab2d0fa06e18b73c5e1a21fa1b384cd6c97e736
UmutAkkaya <uba1995a@gmail.com>
2015-11-29 15:15:25 -0500

    added more into mineCraftServer Schema

--- models/mineCraftServer.js
+++ models/mineCraftServer.js
@@ -5,7 +5,27 @@ var mongoose = require('mongoose');
  */
 var mineCraftServer = mongoose.Schema({
     ip: String,
-    port: Number
+    domain: String,
+    port: String,
+    url: String,
+    info: {
+        name: String,
+        platform: String,
+        version: String, //ex. 1.8.0
+        type: String, //ex. SMP
+        maxPlayerNumber: Number
+    },
+    dateAdded: String,
+    geo: {
+        country: String,
+        city: String
+    },
+    lastUpdate: String,
+    upTime: Number,
+    online: Number,
+    offline: Number,
+    software: String, //ex. CraftBukkit on Bukkit 1.8.8-R0.1-SNAPSHOT
+    plugins: [String]
 });
 
 module.exports = mongoose.model('MineCraftServer', mineCraftServer);
