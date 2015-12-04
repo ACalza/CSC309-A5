69bfdd2e2f0102c2418de0b5ed1223708e44cd62
UmutAkkaya <uba1995a@gmail.com>
2015-12-04 18:57:02 -0500

    Added getServer.js file

---
+++ routes/getServer.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var serverDb = require('../models/index').MineCraftServer;
+var Query = require("mcquery");
+var mcData = require("minecraft-data");
+
+router.get('/:ip', function (req, res) {
+    db.find({
+        ip: req.params.ip
+    }, function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.send('500 Internal Server Error');
+        } else {
+            var i;
+            for (i = 0; i < result.length; i++) {
+                if String(result[i].port) == "25565" {
+                    res.render('server', {
+                        user: req.session.curUser,
+                        server: result[i]
+                    });
+                } else {
+                    return res.send('Server does not exist');
+                }
+            }
+        }
+    });
+
+});
+
+
+router.get('/:ip/:port', function (req, res) {
+    var ip = req.params.ip
+    var port = req.params.port
+    db.find({
+        ip: ip
+    }, function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.send('500 Internal Server Error');
+        } else {
+            var i;
+            for (i = 0; i < result.length; i++) {
+                if String(result[i].port) == port {
+                    res.render('server', {
+                        user: req.session.curUser,
+                        server: result[i]
+                    });
+                } else {
+                    return res.send('Server does not exist');
+                }
+            }
+        }
+    });
+
+});
+
+
+module.exports = router;
