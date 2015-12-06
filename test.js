cde9e6be257f0467438088480abca157c5099328
ACalza <albert.calzaretto@gmail.com>
2015-12-05 19:23:08 -0500

    Added a couple of test cases for Mocha

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
 //Unit tests
 var assert = require("assert");
+var http = require('http');
+var server = require('./server');
 
-describe("Backend Tests on /server/", function(){
-    it('/like/:server_id return not logged in JSON', function(done){
-        
-    })
-})
+
+describe("Backend Tests on /server/", function() {
+    before(function() {
+		server.listen(3000);
+	});
+	after(function() {
+		server.close();
+	});
+
+    it('/like/:server_id header return 530', function(done) {
+        http.get('http://127.0.0.1:3000', function(response) {
+            assert.equal(response.statusCode, 530);
+        });
+        done();
+    });
+    it('/list header return 200', function(done) {
+        http.get('http://127.0.0.1:3000', function(response) {
+            assert.equal(response.statusCode, 200);
+        });
+        done();
+    });
+});
