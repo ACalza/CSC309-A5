5eec4a47a3676bd580f34ac1b94d59c787425008
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-12-05 20:44:13 -0500

    Fix bug in listing all comments (no longer check for comment text)

--- routes/server.js
+++ routes/server.js
@@ -26,8 +26,7 @@ router.post('/create', function (req, res) {
             res.render('createServer', {
                 error: error503
             });
-        }
-        else if (result != 0) {
+        } else if (result != 0) {
             res.status(409)
             res.render('createServer', {
                 error: 'Status 409, This server is already registered'
@@ -86,7 +85,7 @@ router.get('/list', function (req, res, next) {
 });
 
 router.get('/comment/list/:server_id', function (req, res, next) {
-    if (!req.params.server_id || !req.body.text) {
+    if (!req.params.server_id) {
         res.status(503);
         return res.render('error', {
             message: error503
@@ -136,7 +135,7 @@ router.get('/like/:server_id', function (req, res) {
             });
         } else {
             User.findById(req.session.curUser._id, function (err, userModel) {
-                if(userModel.likes.indexOf(new String(req.params.server_id).valueOf()) !== -1){
+                if (userModel.likes.indexOf(new String(req.params.server_id).valueOf()) !== -1) {
                     res.status(304);
                     return res.render('error', {
                         message: "You have already liked this server!"
@@ -241,7 +240,7 @@ function recomendationRecursion(index, maxRecomendations, req, res) {
             //go through each server MCQuery issues so ; for nodemon D:
             for (var i = 0; i < req.body.servers.length; i++) {
                 var rank = 0
-                if(curUser.likes.indexOf(new String(req.body.servers[i]._id).valueOf()) !== -1) {
+                if (curUser.likes.indexOf(new String(req.body.servers[i]._id).valueOf()) !== -1) {
                     continue;
                 };
 
